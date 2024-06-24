f951e5cb9fec5851ee97a4a0360e54657c2478f3
Abhishek Yelley <yabhishek.19.cse@anits.edu.in>
2024-06-24 11:31:00 +0530

    upd: searchfield now uses forms and actions as opposed to manual navigate hook

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Form } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 export default function SearchField() {
     const [inputField, setInputField] = useState('');
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     function handleInputChange(e) {
         setInputField(e.target.value);
         // navigate(`/?query=${e.target.value}`);
     }
 
+    /*
     function handleSubmit(e) {
         e.preventDefault();
         navigate(`/?query=${inputField}`);
     }
+    */
 
     return (
         <div className='SearchField'>
@@ -24,12 +27,12 @@ export default function SearchField() {
             }} >
                 <label
                     className="TextLabel"
-                    htmlFor="searchField"
+                    htmlFor="query"
                 >
                     Search Field
                 </label>
             </div>
-            <form onSubmit={handleSubmit}>
+            <Form method="get" action="/" >
                 <div style={{
                     display: "flex",
                     justifyContent: "center",
@@ -37,16 +40,16 @@ export default function SearchField() {
                 }} >
                     <input
                         className="TextInputField"
-                        id="searchField"
+                        id="query"
                         placeholder={"Type here..."}
                         type="text"
-                        name="searchField"
+                        name="query"
                         value={inputField}
                         onChange={handleInputChange}
                     />
                     <button className="SearchBtn" type="submit">Submit</button>
                 </div>
-            </form>
+            </Form>
         </div>
     );
 }
