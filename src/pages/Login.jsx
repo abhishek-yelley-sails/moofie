71a4544662e20f4e0ed87bcaf52a6941ca17d5b5
Abhishek Yelley <abhishek.yelley@sailssoftware.com>
2024-06-26 19:05:10 +0530

    add: elements to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, Form } from "react-router-dom";
+import { useState } from "react";
+import { Input, Button } from "@mui/material";
 
 export default function Login() {
+    const [formValues, setFormValues] = useState({
+        email: "",
+        password: "",
+    });
+
+    function handleChange(e) {
+        setFormValues(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }));
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
-            <Link to="/signup">Do no have an account? Sign Up!</Link>
+        <div className="LoginFormContainer">
+            <h1 style={{color: "black"}}>Log In</h1>
+            <Form method="post" style={{ color: "white" }} className="LoginForm">
+
+                <Input sx={{fontWeight: 600}} placeholder="Email" type="email" name="email" value={formValues.email} onChange={handleChange} />
+                <br/>
+
+                <Input sx={{fontWeight: 600}} placeholder="Password" type="password" name="password" value={formValues.password} onChange={handleChange} />
+                <br/>
+
+                <br/>
+                <Button sx={{width: "max-content", alignSelf: "center"}} type="submit" variant="contained">Log In</Button>
+                <br/>
+                {/* <button type="submit">Sign up</button> */}
+            </Form>
+            <Link style={{color: "black", fontWeight: 500}} to="/signup">Do not have an account? Sign Up</Link>
         </div>
     );
 }
