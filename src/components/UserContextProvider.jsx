bb9ce2ab534b3ac38ca798f536c0976cab16567e
Abhishek Yelley <abhishek.yelley@sailssoftware.com>
2024-06-27 14:18:24 +0530

    del: theme options

--- src/components/UserContextProvider.jsx
+++ src/components/UserContextProvider.jsx
@@ -2,8 +2,6 @@ import { createContext, useState } from "react";
 import PropTypes from 'prop-types';
 
 export const UserContext = createContext({
-    theme: "",
-    toggleTheme: () => { },
     name: "",
     changeName: () => { },
     isLoggedIn: false,
@@ -13,14 +11,10 @@ export const UserContext = createContext({
 });
 
 export default function UserContextProvider({ children }) {
-    const [theme, setTheme] = useState("light");
     const [name, setName] = useState("test");
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [image, setImage] = useState("/defaultProfileImage.webp");
 
-    function toggleTheme() {
-        setTheme(prev => prev === "light" ? "dark" : "light");
-    }
     function changeName(newName) {
         setName(newName);
     }
@@ -32,8 +26,6 @@ export default function UserContextProvider({ children }) {
     }
 
     const ctxValue = {
-        theme,
-        toggleTheme,
         name,
         changeName,
         isLoggedIn,
