5efa2aa1a2e32a6137e3045db99b795fe0f7826e
Abhishek Yelley <abhishek.yelley@sailssoftware.com>
2024-06-27 14:17:56 +0530

    upd: change navigation bar to mui AppBar

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,33 +1,193 @@
 import { NavLink } from 'react-router-dom';
 import { useContext } from 'react';
-import { UserContext } from './UserContextProvider';
+import { UserContext } from './UserContextProvider.jsx';
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const pages = [{ name: "About", link: "/about" }];
+const settings = [{ name: "Profile", link: "/profile" }, { name: "Logout", link: "/logout" }];
 
 export default function MainNavigation() {
-    const navlinkClass = (isActive, isPending) => (isPending ? "pending" : isActive ? "active" : "");
     const ctxValue = useContext(UserContext);
-    return (
-        <header>
-            <nav>
-                <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", padding: "0px" }}>
-                    <ul className="nav-ul">
-                        <li>
-                            <NavLink to="/" className={({ isActive, isPending }) => ("nav-ul " + navlinkClass(isActive, isPending))} end>Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/about" className={({ isActive, isPending }) => ("nav-ul " + navlinkClass(isActive, isPending))} end>About</NavLink>
-                        </li>
-                    </ul>
-                    {ctxValue.isLoggedIn &&
-                        <div style={{ display: "flex", alignItems: "center" }}>
-                            <span style={{ fontWeight: 500 }}>Hello, {ctxValue.name}!</span>
-                            <NavLink to="/user" className={({ isActive, isPending }) => ("nav-img " + navlinkClass(isActive, isPending))} end>
-                                <img style={{ width: "50px", height: "50px", padding: "0px", margin: "0px", borderRadius: "50%" }} src={ctxValue.image} />
-                            </NavLink>
-                        </div>
-                    }
-                </div>
-            </nav>
 
-        </header>
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="sticky" sx={{ background: "rgb(41, 61, 69)" }}>
+            <nav>
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' },
+                                // fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            <NavLink
+                                to={"/"}
+                                className={({ isActive, isPending }) => (isPending ? "main-pending" : isActive ? "main-active" : "")}
+                            >
+                                MOOFIE
+                            </NavLink>
+                        </Typography>
+
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center">
+                                            <NavLink
+                                                style={{ textDecoration: "none", color: "black", padding: "5px" }}
+                                                to={page.link}
+                                                className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")}
+                                            >
+                                                {page.name}
+                                            </NavLink>
+                                        </Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                        <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'flex', md: 'none' },
+                                flexGrow: 1,
+                                // fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            <NavLink
+                                to={"/"}
+                                className={({ isActive, isPending }) => (isPending ? "main-pending" : isActive ? "main-active" : "")}
+                            >
+                                MOOFIE
+                            </NavLink>
+                        </Typography>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            {pages.map((page) => (
+                                <NavLink
+                                    to={page.link}
+                                    key={page.name}
+                                    onClick={handleCloseNavMenu}
+                                    style={{ marginRight: "2px", marginLeft: "2px", display: 'block', textDecoration: "none" }}
+                                >
+                                    {page.name}
+                                </NavLink>
+                            ))}
+                        </Box>
+
+                        {ctxValue.isLoggedIn &&
+                            <Box sx={{ flexGrow: 0 }}>
+                                <Tooltip title="Open settings">
+                                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                        <Avatar alt="User" src="/defaultProfileImage.webp" />
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    sx={{ mt: '45px' }}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
+                                >
+                                    {settings.map((setting) => (
+                                        <MenuItem key={setting.name} onClick={handleCloseUserMenu}>
+                                            <Typography textAlign="center">
+                                                <NavLink
+                                                    style={{ textDecoration: "none", color: "black", padding: "5px" }}
+                                                    to={setting.link}
+                                                    className={({ isActive, isPending }) => (isPending ? "main-pending" : isActive ? "main-active" : "")}
+                                                >
+                                                    {setting.name}
+                                                </NavLink>
+                                            </Typography>
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </Box>
+                        }
+                    </Toolbar>
+                </Container>
+            </nav>
+        </AppBar>
     );
 }
