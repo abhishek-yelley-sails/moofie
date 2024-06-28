e8be17bdd915c96eaddb76be6702ffc921aad4cd
Abhishek Yelley <abhishek.yelley@sailssoftware.com>
2024-06-29 00:34:36 +0530

    add: pagination

--- src/loaders/searchResultsLoader.js
+++ src/loaders/searchResultsLoader.js
@@ -1,8 +1,9 @@
 export default function searchResultsLoader({ request }) {
     const url = new URL(request.url);
     const query = url.searchParams.get("query");
+    const page = parseInt(url.searchParams.get("page"));
 
-    const fetchUrl = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+    const fetchUrl = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=${page?page:1}`;
     const options = {
         method: 'GET',
         headers: {
--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,33 +1,64 @@
 import MovieResults from "../components/MovieResults.jsx";
 import MovieResultsLoading from "../components/MovieResultsLoading.jsx";
-import { useSearchParams, useLoaderData, Await } from "react-router-dom";
-import { Suspense } from 'react';
+import { useSearchParams, useLoaderData, Await, useNavigate } from "react-router-dom";
+import { Suspense, useState, useEffect } from 'react';
+import Pagination from '@mui/material/Pagination';
 
 export default function Results() {
     const [searchParams] = useSearchParams();
+    const [page, setPage] = useState(1);
+    const navigate = useNavigate();
     const query = searchParams.get("query");
     const { data } = useLoaderData();
-    if(!query) {
+
+    useEffect(() => {
+        const currentPage = searchParams.get("page");
+        if (currentPage) {
+            setPage(parseInt(currentPage, 10));
+        }
+    }, [searchParams]);
+
+    function handlePageChange(e, value) {
+        console.log(value);
+        setPage(value);
+        navigate(`?query=${query}&page=${value}`);
+    }
+
+    if (!query) {
         return (
             <div className='MovieResults Center'>
                 <h1>No query provided!</h1>
-            </div>    
+            </div>
         )
     }
     return (
-        <div className='MovieResults Center'>
-            <Suspense fallback={<MovieResultsLoading results={20} />}>
+            <Suspense fallback={<div className='MovieResults Center'><MovieResultsLoading results={20} /></div>}>
                 <Await
                     resolve={data}
                     errorElement={
-                        <div>Could not load movies</div>
+                        <div className="MovieResults Center">Could not load movies</div>
                     }
                 >
                     {(data) => (
-                        <MovieResults data={data} />
+                        <>
+                            <div className='MovieResults Center'>
+                                <MovieResults data={data} />
+                            </div>
+                            <div style={{display: "flex", justifyContent: "center"}}>
+                            <div style={{margin: "30px"}}>
+                            <Pagination
+                                onChange={handlePageChange}
+                                count={data.total_pages}
+                                page={page}
+                                color="primary"
+                                showFirstButton
+                                showLastButton
+                            />
+                            </div>
+                            </div>
+                        </>
                     )}
                 </Await>
             </Suspense>
-        </div>
     );
 }
